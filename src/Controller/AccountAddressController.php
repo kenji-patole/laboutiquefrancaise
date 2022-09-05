d224fe350ee9aa129c4482ca021acf745eba2c53
kenji-patole <kenjisupremacy@gmail.com>
2022-09-04 20:42:33 -0400

    Entité Addresse + Ajouter, Modifier, Supprimer une addresse 🚑️

---
+++ src/Controller/AccountAddressController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Address;
+use App\Form\AddressType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccountAddressController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+    
+
+    #[Route('/compte/adresses', name: 'account_address')]
+    public function index(): Response
+    {
+        return $this->render('account/address.html.twig');
+    }
+
+
+    #[Route('/compte/ajouter-une-adresse', name: 'account_address_add')]
+    public function add(Request $request): Response
+    {   
+        $address = new Address;
+
+        // Je passe en paramètres à ma fonction createForm() le type du formulaire et l'objet
+        $form = $this->createForm(AddressType::class, $address);
+
+        // Ecoute la requête
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $address->setUser($this->getUser());
+
+            // Fige la data
+            $this->entityManager->persist($address);
+
+            // Exécute
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('account_address');
+        }
+
+        return $this->render('account/address_form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    #[Route('/compte/modifier-une-adresse/{id}', name: 'account_address_edit')]
+    public function edit(Request $request, $id): Response
+    {   
+        $address = $this->entityManager->getRepository(Address::class)->findOneById($id);
+
+        // S'il n'y a aucune adresse OU que l'utilisateur ne correspond pas à celui actuellement connecté
+        if (!$address || $address->getUser() != $this->getUSer()) {
+            return $this->redirectToRoute('account_address');
+        }
+
+        // Je passe en paramètres à ma fonction createForm() le type du formulaire et l'objet
+        $form = $this->createForm(AddressType::class, $address);
+
+        // Ecoute la requête
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            // Exécute
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('account_address');
+        }
+
+        return $this->render('account/address_form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    #[Route('/compte/supprimer-une-adresse/{id}', name: 'account_address_delete')]
+    public function delete($id): Response
+    {   
+        $address = $this->entityManager->getRepository(Address::class)->findOneById($id);
+
+        // S'il y a une adresse ET que l'utilisateur correspond à celui actuellement connecté
+        if ($address || $address->getUser() == $this->getUSer()) {
+
+            $this->entityManager->remove($address);
+            
+            // Exécute
+            $this->entityManager->flush();
+        }
+
+    
+        return $this->redirectToRoute('account_address');
+
+    }
+}
